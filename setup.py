95a66cc94c917973432f1dea59e5190624d6fc7a
Laharah <laharah22@gmail.com>
2015-01-22 17:26:15 -0800

    - 0.5 MILESTONE! TECHNICALLY WORKING!!!!

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ from setuptools import setup
 __plugin_name__ = "FileBotTool"
 __author__ = "laharah"
 __author_email__ = "laharah+fbt@gmail.com"
-__version__ = "0.1"
+__version__ = "0.5"
 __url__ = ""
 __license__ = "GPLv3"
 __description__ = ""
