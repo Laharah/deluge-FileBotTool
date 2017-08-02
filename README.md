cc61a5011c2151b94668f6cbd4a64a4813d85015
laharah <laharah22@gmail.com>
2017-08-01 18:14:26 -0700

    Added fixes for #33 and #34
    - ubuntu doesen't like `snap` apps to share tmp files, workarounds are
      now limited to just windows os'

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FilebotTool: FileBot Integration for Deluge
 *requires [Filebot](http://www.filebot.net/)*
 
-**Version 1.1.8**
+**Version 1.1.10**
 
 ##### [Download Latest Release](https://github.com/Laharah/deluge-FilebotTool/releases/latest)
 
--- filebottool/pyfilebot.py
+++ filebottool/pyfilebot.py
@@ -20,7 +20,7 @@ FILEBOT_EXE = spawn.find_executable('filebot')
 if not FILEBOT_EXE:
     locations = [
         r'C:\Program Files\FileBot\filebot.exe', r'/usr/bin/filebot',
-        r'/usr/local/bin/filebot'
+        r'/usr/local/bin/filebot', r'/snap/bin/filebot'
     ]
     for loc in locations:
         if os.path.exists(loc):
@@ -141,7 +141,8 @@ def rename(targets,
         language_code=language_code)
 
     # TODO:better error handling
-    exit_code, data, filebot_error = _execute(filebot_arguments)
+    workaround = True if os.name == 'nt' else False
+    exit_code, data, filebot_error = _execute(filebot_arguments, workaround)
 
     if exit_code != 0:
         raise FilebotRuntimeError("FILEBOT OUTPUT DUMP:\n{0}".format(data))
@@ -546,7 +547,7 @@ def _build_script_arguments(script_name, script_arguments):
     return process_arguments
 
 
-def _execute(process_arguments, workaround=True):
+def _execute(process_arguments, workaround=False):
     """underlying execution method to call filebot as subprocess
 
     Handles the actual execution and output capture
